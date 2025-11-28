bd236b959edd89355059a7050b60416c9a8a4558
beritakobar-tech <237912189+beritakobar-tech@users.noreply.github.com>
2025-11-29 01:53:02 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,39 @@
+package com.beritakobar.beritakobarmobile
+
+import android.os.Bundle
+import androidx.webkit.Webview
+import androidx.webkit.WebviewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BeritaKobarMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BeritaKobarMobileTheme {
+        Greeting("Android")
+    }
+}
